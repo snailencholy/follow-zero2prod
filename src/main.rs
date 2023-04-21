9f6ca5fc05fcbaa75ff5be94333edde895f6c521
snailencholy <paul.hampton1992@gmail.com>
2023-04-21 07:35:05 -0500

    Integration testing and 2 endpoints are set up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+//! src/main.rs
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
