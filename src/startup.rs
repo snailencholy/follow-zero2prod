f56391bc46b0eaff8dee98d79b339fdb9ed7604e
snailencholy <paul.hampton1992@gmail.com>
2023-04-22 13:28:20 -0500

    Added init file, db support, and refactored src folder

---
+++ src/startup.rs
@@ -0,0 +1,21 @@
+//! src/startup.rs
+
+use crate::routes::{health_check, subscribe};
+
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    //.bind(address)?
+    .listen(listener)?
+    .run();
+    // No .await
+    Ok(server)
+}
